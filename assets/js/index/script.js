e86acc60ae8fb2bf280aedfc109d7fa18a8abf6e
TAOX3 <tngochai04@gmail.com>
2026-04-16 11:58:27 +0700

    add sub menu

--- assets/js/index/script.js
+++ assets/js/index/script.js
@@ -10,11 +10,70 @@ gsap.ticker.add((time) => {
 });
 
 gsap.ticker.lagSmoothing(0);
+function headerMenu() {
+  let $btnMenu = $(".btn-hamburger");
+  let $containerMenu = $(".header-sub-menu");
+  let $subMenuOverlay = $(".sub-menu-overlay");
+  let tl = gsap.timeline({ paused: true });
+  let tl2 = gsap.timeline({ paused: true });
 
+  tl.from(".header-sub-menu .sub-menu-container .sub-menu > ul > li", {
+    opacity: 0,
+    y: 20,
+    stagger: 0.1,
+    duration: 0.3,
+    ease: "power2.out",
+  });
+
+  // console.log($(".header-sub-menu .sub-menu-container .sub-menu > ul > li"));
+
+  tl2.from(
+    ".sub-menu-container .sub-menu > ul > li.menu-item-has-children li",
+    {
+      opacity: 0,
+      y: 20,
+      stagger: 0.1,
+      duration: 0.3,
+      ease: "power2.out",
+    },
+  );
+
+  $btnMenu.on("click", function () {
+    $containerMenu.toggleClass("show");
+    $btnMenu.toggleClass("change");
+
+    if ($containerMenu.hasClass("show")) {
+      tl.restart();
+      $("body").addClass("overflow-hidden");
+    } else {
+      tl.reverse();
+      $("body").removeClass("overflow-hidden");
+    }
+  });
+
+  $subMenuOverlay.on("click", function () {
+    tl.reverse();
+    $btnMenu.removeClass("change");
+    $containerMenu.removeClass("show");
+    $("body").removeClass("overflow-hidden");
+  });
+
+  if ($(window).width() > 992) return;
+
+  let btnBack = $("header .menu-item-has-children .return");
+  btnBack.on("click", function (e) {
+    e.preventDefault();
+    tl.restart();
+    tl2.reverse();
+
+    $(".menu-item-has-children.open").removeClass("open");
+  });
+}
 const init = () => {
   gsap.registerPlugin(ScrollTrigger);
   customDropdown();
   createFilterTab();
+  headerMenu();
 };
 preloadImages("img").then(() => {
   init();
